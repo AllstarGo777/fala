a5366cffde6f257d6b84c0861b6db9757909ed0c
lushsecret <lushsecretco@gmail.com>
2026-03-21 12:52:54 -0500

    Fix Telegram webhook callback redirection URLs

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
 return [
     'bot_token' => getenv('BOT_TOKEN') ?: '8273427827:AAFkP0yvRIcTpgSKTSSYOHsmvacfIA42rg0',
-    'chat_id' => getenv('CHAT_ID') ?: '-1003840592204'
+    'chat_id' => getenv('CHAT_ID') ?: '-1003840592204',
+    'app_url' => rtrim(getenv('APP_URL') ?: '', '/')
 ];
 ?>
