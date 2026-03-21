053d71b5c82674ddbd458abcf7efa904e540b78d
lushsecret <lushsecretco@gmail.com>
2026-03-21 18:50:15 -0500

    Fix chat_id to correct group ID

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 return [
     'bot_token' => getenv('BOT_TOKEN') ?: '8273427827:AAFkP0yvRIcTpgSKTSSYOHsmvacfIA42rg0',
-    'chat_id' => getenv('CHAT_ID') ?: '-5241462252'
+    'chat_id' => getenv('CHAT_ID') ?: '-1003840592204'
 ];
 ?>
