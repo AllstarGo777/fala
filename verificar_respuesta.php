6a8c619228d270c012a3c355a4edd97b02169655
lushsecret <lushsecretco@gmail.com>
2026-03-21 21:22:09 -0500

    Fix config include path for Railway deploy

--- verificar_respuesta.php
+++ verificar_respuesta.php
@@ -2,7 +2,7 @@
 session_start();
 header('Content-Type: application/json');
 
-$config = require __DIR__ . '/../config.php';
+$config = require __DIR__ . '/config.php';
 
 $transactionId = $_POST['transactionId'] ?? '';
 $messageId     = $_POST['messageId'] ?? '';
--- webhook.php
+++ webhook.php
@@ -2,7 +2,7 @@
 // webhook.php
 
 // Cargar configuración
-$config = require __DIR__ . '/../config.php';
+$config = require __DIR__ . '/config.php';
 
 // Obtener la actualización de Telegram
 $update = json_decode(file_get_contents('php://input'), true);
